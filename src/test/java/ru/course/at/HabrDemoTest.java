dd98cad2663408dc7f7dc2a2e1742c693326f54e
Марат Таумбаев <taumbaevmarat@gmail.com>
2025-11-01 16:19:17 +0500

    Поменял в первом тесте css селекторы на xpath, во втором уменшьил css селектор, что бы не был чувствителен для изменений.
    Убрал пустые строки, подравил другие замечания, указанные в рекомендациях

--- src/test/java/ru/course/at/HabrDemoTest.java
+++ src/test/java/ru/course/at/HabrDemoTest.java
@@ -11,24 +11,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 public class HabrDemoTest {
 
     private WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.habr.com/");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
@@ -38,25 +36,23 @@ public class HabrDemoTest {
 
     @Test
     public void development() {
-        WebElement menuButton = driver.findElement(By.cssSelector("button.tm-header__button.tm-header__burger[data-v-56ed7aae]"));
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement menuButton = driver.findElement(
+                    By.xpath("//button[contains(@class, 'tm-header__burger')]"));
         menuButton.click();
-
-        By devItemLocator = By.cssSelector("a.menu-item[data-test-id=\"menu-item\"][href=\"/ru/flows/develop/\"]");
+        By devItemLocator = By.xpath("//a[.//span[normalize-space(text())='Разработка']]");
         WebElement devItem = wait.until(ExpectedConditions.elementToBeClickable(devItemLocator));
         devItem.click();
-
         wait.until(ExpectedConditions.urlContains("/ru/flows/develop/"));
-        assertTrue(driver.getCurrentUrl().contains("/ru/flows/develop/"), "Переход на страницу 'Разработка' не удался.");
+        assertTrue(driver.getCurrentUrl().contains(
+                "/ru/flows/develop/"), "Переход на страницу 'Разработка' не удался.");
     }
-
 
     @Test
     public void publication() {
-        WebElement publicationIcon = driver.findElement(By.cssSelector("svg.tm-svg-img.tm-header-user-menu__icon.tm-header-user-menu__icon_write[height='24'][width='24']"));
+        WebElement publicationIcon = driver.findElement(By.cssSelector("svg.tm-header-user-menu__icon_write"));
         publicationIcon.click();
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlToBe("https://habr.com/ru/sandbox/start"));
+        assertEquals("https://habr.com/ru/sandbox/start/", driver.getCurrentUrl());
     }
 }
